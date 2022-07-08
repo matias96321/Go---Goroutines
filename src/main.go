0ffe341d5ee3235542d07603b1ae9ee432141530
matthiass2 <matias_69914081@hotmail.com>
2022-07-08 19:36:08 -0300

    feat: example of Goroutines

--- src/main.go
+++ src/main.go
@@ -1,5 +1,23 @@
-package src
+package main
+
+import "fmt"
 
 func main() {
+	// A "channel"
+	ch := make(chan string)
 
+	// Start concurrent routines
+	go push("Moe", ch)
+	go push("Larry", ch)
+	go push("Curly", ch)
+
+	// Read 3 results
+	// (Since our goroutines are concurrent,
+	// the order isn't guaranteed!)
+	fmt.Println(<-ch, <-ch, <-ch)
+}
+
+func push(name string, ch chan string) {
+	msg := " Hey, " + name + ".  "
+	ch <- msg
 }
